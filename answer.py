11a34d5385b6b4ae182f06a763270c027400cfb2
sandygoreraza <gorerazasandy10@gmail.com>
2022-12-18 13:55:04 +0200

    Create answer.py
    answer to the python challenge

---
+++ answer.py
@@ -0,0 +1,20 @@
+import time
+number = int(input())
+
+if(number < 1):
+   print("Invalid number entered")
+while number > 1:
+      if(number % 2)==0:
+        number = number / 2
+        steps +=1
+        print(number)
+      else:
+           number = number * 3 + 1
+           print(number)
+           steps +=1
+print("steps =" + str(steps))         
+       
+  
+ 
+
+    
